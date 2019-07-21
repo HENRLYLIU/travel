498f2474a22870380ab37711ddc23d6d95dea8e1
henrly <1695983436@qq.com>
2019-07-21 15:56:14 +0800

    登录功能完成，下一步修改header页面中的欢迎语句

--- travel/src/main/java/cn/itcast/travel/dao/impl/UserDaoImpl.java
+++ travel/src/main/java/cn/itcast/travel/dao/impl/UserDaoImpl.java
@@ -42,20 +42,21 @@ public class UserDaoImpl implements UserDao {
                 user.getEmail(),
                 user.getStatus(),
                 user.getCode()
-                );
+        );
     }
 
     /**
      * 根据激活码查询用户对象
+     *
      * @param code
      * @return
      */
     @Override
     public User findByCode(String code) {
-        User user=null;
+        User user = null;
         try {
-            String sql="select * from tab_user where code=?";
-            user=template.queryForObject(sql,new BeanPropertyRowMapper<User>(User.class),code);
+            String sql = "select * from tab_user where code=?";
+            user = template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), code);
         } catch (DataAccessException e) {
             e.printStackTrace();
         }
@@ -64,13 +65,36 @@ public class UserDaoImpl implements UserDao {
 
     /**
      * 修改指定用户激活状态
+     *
      * @param user
      */
     @Override
     public void updateStatus(User user) {
         //1.更具激活码查询用户对象
-        String sql="update tab_user set status='Y' where uid=?";
-        template.update(sql,user.getUid());
+        String sql = "update tab_user set status='Y' where uid=?";
+        template.update(sql, user.getUid());
+    }
+
+    /**
+     * 根据用户名和密码查询
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    @Override
+    public User findByUsernameAndPassword(String username, String password) {
+        User user = null;
+        try {
+            //1.定义sql
+            String sql = "select * from tab_user where username = ? and password=?";
+            //2.执行sql
+            user = template.queryForObject(sql, new BeanPropertyRowMapper<User>(User.class), username, password);
+        } catch (Exception e) {
+
+        }
+
+        return user;
     }
 
 }
